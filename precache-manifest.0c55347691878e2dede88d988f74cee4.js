76b235919db3e5fa5eb65016751ecf416138e810
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2020-11-11 17:28:29 +0300

    Updates

---
+++ precache-manifest.0c55347691878e2dede88d988f74cee4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb0588de2e8b8c441c6365d81419ba1",
+    "url": "/reactWaySamurai/index.html"
+  },
+  {
+    "revision": "f1eb0635a4f4d27a388c",
+    "url": "/reactWaySamurai/static/css/3.caf31ad6.chunk.css"
+  },
+  {
+    "revision": "9a0d6cf169f4bd88cd64",
+    "url": "/reactWaySamurai/static/css/4.c94a14ac.chunk.css"
+  },
+  {
+    "revision": "ff31021b45db63a3a2d1",
+    "url": "/reactWaySamurai/static/css/main.69160dd4.chunk.css"
+  },
+  {
+    "revision": "2f62cb530324bcd439d2",
+    "url": "/reactWaySamurai/static/js/2.1c4f1ca2.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/reactWaySamurai/static/js/2.1c4f1ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1eb0635a4f4d27a388c",
+    "url": "/reactWaySamurai/static/js/3.45916bd7.chunk.js"
+  },
+  {
+    "revision": "9a0d6cf169f4bd88cd64",
+    "url": "/reactWaySamurai/static/js/4.eafa2ef7.chunk.js"
+  },
+  {
+    "revision": "ff31021b45db63a3a2d1",
+    "url": "/reactWaySamurai/static/js/main.c95a8421.chunk.js"
+  },
+  {
+    "revision": "6ebb343539411e9d7b08",
+    "url": "/reactWaySamurai/static/js/runtime-main.72c45e99.js"
+  },
+  {
+    "revision": "59ff89f94eb92eb75cc519d80477aaf0",
+    "url": "/reactWaySamurai/static/media/arni.59ff89f9.png"
+  },
+  {
+    "revision": "ffa50fc4831501055232883311005d95",
+    "url": "/reactWaySamurai/static/media/facebook-app-logo.ffa50fc4.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/reactWaySamurai/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/reactWaySamurai/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "53b9845e374cd0d1833796779ed80354",
+    "url": "/reactWaySamurai/static/media/internet.53b9845e.svg"
+  },
+  {
+    "revision": "27fc27b682d7147fc0935299c8467922",
+    "url": "/reactWaySamurai/static/media/loading.27fc27b6.svg"
+  },
+  {
+    "revision": "dd90206d4d813b02e3f92ed7373ae817",
+    "url": "/reactWaySamurai/static/media/twitter.dd90206d.svg"
+  },
+  {
+    "revision": "93a57c9f7d6386c36b6805255eac0d81",
+    "url": "/reactWaySamurai/static/media/user.93a57c9f.png"
+  },
+  {
+    "revision": "9eaa3aa7f4f9c1ae745d6649a0fbd8dc",
+    "url": "/reactWaySamurai/static/media/vk.9eaa3aa7.svg"
+  },
+  {
+    "revision": "a6372485318244c18a497ae9a9b62467",
+    "url": "/reactWaySamurai/static/media/youtube.a6372485.svg"
+  }
+]);
